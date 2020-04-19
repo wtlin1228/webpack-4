93b534b14dc935a8264e78289deb8ffc3932e295
wtlin1228 <wtlin1228@gmail.com>
2020-04-19 15:55:10 +0800

    add webpack preset compression, add webpack preset analyzer

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-style";
 import makeImage from "./image";
-
 import css from "./footer.css";
 import buttonStyles from "./button.css";
 
@@ -12,6 +11,7 @@ import imageUrl from "./webpack-logo.jpg";
 const image = makeImage(imageUrl);
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
+
 document.body.appendChild(button);
 document.body.appendChild(image);
 document.body.appendChild(footer);
